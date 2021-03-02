d37c62a11163bf23f4d0a635690bd4a59d8074b1
Matt Bommer <matthew.bommer@willowtreeapps.com>
2021-03-02 16:05:36 -0600

    Re-created detail screen and working on making a xib for pokemon base stats.

--- Pokedex/Pokedex/PokemonDetailViewController.swift
+++ Pokedex/Pokedex/PokemonDetailViewController.swift
@@ -12,10 +12,12 @@ class PokemonDetailViewController: UIViewController {
 
     
     @IBOutlet weak var pokemonSplashImage: UIImageView!
-    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
+    @IBOutlet weak var hpBar: UIProgressView!
     var pokemon: Pokemon!
 
     override func viewDidLoad() {
@@ -24,21 +26,32 @@ class PokemonDetailViewController: UIViewController {
     }
     
     func configure() {
-        nameLabel.text = pokemon.name.capitalized
+        navigationItem.title = pokemon.name.capitalized
+        idLabel.text = pokemonIDStyler()
         heightLabel.text = "Height: \(pokemon.height)"
         weightLabel.text = "Weight: \(pokemon.weight)"
         
         let types = pokemon.types.map { $0.typeName }.joined(separator: ", ")
         typeLabel.text = "Type(s): \(types)"
         
+        let abilities = pokemon.abilities.map { $0.name }.joined(separator: ", ")
+        abilitiesLabel.text = "Abilities: \(abilities)"
+        
         guard let url = URL(string: pokemon.sprites.splash) else {
             return
         }
-        
-        let frame = CGSize(width: 200, height: 200)
-        let placeHolderImage = UIImage(named: "placeholder")
-        let filter = AspectScaledToFillSizeFilter(size: frame)
-        pokemonSplashImage.af.setImage(withURL: url, placeholderImage: placeHolderImage, filter: filter)
+        pokemonSplashImage.af.setImage(withURL: url)
+    }
+    
+    private func pokemonIDStyler() -> String {
+        let id = String(pokemon.id)
+        var placeholderZeros = ""
+        if id.count < 3 {
+            for _ in 0..<(3 - id.count) {
+                placeholderZeros += "0"
+            }
+        }
+        return "#\(placeholderZeros)\(id)"
     }
 }
     
